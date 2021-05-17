fdfddceb6d63a8f297cb8f208396d77653e72242
Jim Bodden <jim@pcshowme.com>
2021-05-16 22:39:54 -0500

    Version 1.41' -- Improvements in fconvertSongTextToChordPro

--- ConvertToChordProFormat.sh
+++ ConvertToChordProFormat.sh
@@ -4,7 +4,7 @@
 #
 #   Bash Script to batch convert songs from generic text to ChordPro format"
 #
-VERSION='Script: ConvertToChordProFormat.sh -- Version 1.39'
+VERSION='Script: ConvertToChordProFormat.sh -- Version 1.41'
 #   Created by Jim Bodden -- 5/14/2021
 #   Modified by Jim Bodden on -- 5/16/2021
 #
@@ -53,9 +53,16 @@ fconvertSongTextToChordPro () # Function converts an individual song file from p
     for FILE in s-*.txt
     do
 	    SOURCE=$FILE; ## SOURCE="FieryLove.txt"
-        cat $SOURCE | sed 's/ A / [A] /g'| sed 's/ Ab/ [Ab] /g'| sed 's/ A#/ [A#] /g' | sed 's/ B / [B] /g'| sed 's/ Bb/ [Bb] /g'| sed 's/ B#/ [B#] /g' | sed 's/ C / [C] /g'| sed 's/ Cb/ [Cb] /g'| sed 's/ C#/ [C#] /g' | sed 's/ D / [D] /g'| sed 's/ Db/ [Db] /g'| sed 's/ D#/ [D#] /g' | sed 's/ E / [E] /g'| sed 's/ Eb/ [Eb] /g'| sed 's/ E#/ [E#] /g' | sed 's/ F / [F] /g'| sed 's/ Fb/ [Fb] /g'| sed 's/ F#/ [F#] /g' | sed 's/ G / [G] /g'| sed 's/ Gb/ [Gb] /g'| sed 's/ G#/ [G#] /g' > FL1.txt
-        cat FL1.txt | sed ':a;N;$!ba;s/ A\n/ [A] \n /g'| sed ':a;N;$!ba;s/ B\n/ [B] \n /g'| sed ':a;N;$!ba;s/ C\n/ [C] \n /g' | sed ':a;N;$!ba;s/ D\n/ [D] \n /g'| sed ':a;N;$!ba;s/ E\n/ [E] \n /g'| sed ':a;N;$!ba;s/ F\n/ [F] \n /g'| sed ':a;N;$!ba;s/ G\n/ [G] \n /g'| sed 's/ – / [-] /g' | sed 's/ : / [:] /g' | sed 's/ :: / [::] /g'  > FL2.txt
-        cat FL2.txt | sed 's/-://' > FL1.txt; cat FL1.txt | sed 's/-/[-]/g' > FL2.txt
+        A="sed 's/A / [A] /g'| sed 's/Ab/ [Ab] /g'| sed 's/A#/ [A#] /g'| sed ':a;N;$!ba;s/ A\n/ [A] \n /g'"
+        B="sed 's/B / [B] /g'| sed 's/Bb/ [Bb] /g'| sed 's/B#/ [B#] /g'| sed ':a;N;$!ba;s/ B\n/ [B] \n /g'"
+        C="sed 's/C / [C] /g'| sed 's/Cb/ [Cb] /g'| sed 's/C#/ [C#] /g'| sed ':a;N;$!ba;s/ C\n/ [C] \n /g'"
+        D="sed 's/D / [D] /g'| sed 's/Db/ [Db] /g'| sed 's/D#/ [D#] /g'| sed ':a;N;$!ba;s/ D\n/ [D] \n /g'"
+        E="sed 's/E / [E] /g'| sed 's/Eb/ [Eb] /g'| sed 's/E#/ [E#] /g'| sed ':a;N;$!ba;s/ E\n/ [E] \n /g'"
+        F="sed 's/F / [F] /g'| sed 's/Fb/ [Fb] /g'| sed 's/F#/ [F#] /g'| sed ':a;N;$!ba;s/ F\n/ [F] \n /g'"
+        G="sed 's/G / [G] /g'| sed 's/Gb/ [Gb] /g'| sed 's/G#/ [G#] /g'| sed ':a;N;$!ba;s/ G\n/ [G] \n /g'"
+        DASH="sed 's/-://'| sed 's/-/[-]/g'"
+        cat $SOURCE | eval $A | eval $B | eval $C | eval $D | eval $E | eval $F | eval $G | eval $DASH > SongChordPro.txt 
+        
         C=0; IFS=$'\n'
         while read LINE     
         do
@@ -67,7 +74,7 @@ fconvertSongTextToChordPro () # Function converts an individual song file from p
                 SPLIT1="$( cut -d '[' -f 1 <<< "$LINE" )"; SPLIT2="$( cut -d '[' --complement -s -f1 <<< "$LINE" )"; 
                 echo "$SPLIT1" | xargs>> FL1.txt; echo "[$SPLIT2">> FL1.txt
             fi
-        done < FL2.txt
+        done < ./SongChordPro.txt 
         cp FL1.txt "M-$TITLE.txt"
         echo "Completed!  $C Lines converted from generic text to ChordPro format"
     done
