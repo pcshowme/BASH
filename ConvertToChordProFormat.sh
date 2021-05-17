209358f3c82706884bf3c621d1146ff632e60250
Jim Bodden <jim@pcshowme.com>
2021-05-17 14:22:18 -0500

    Update ConvertToChordProFormat.sh

--- ConvertToChordProFormat.sh
+++ ConvertToChordProFormat.sh
@@ -4,14 +4,14 @@
 #
 #   Bash Script to batch convert songs from generic text to ChordPro format"
 #
-VERSION='Script: ConvertToChordProFormat.sh -- Version 1.41'
+VERSION='Script: ConvertToChordProFormat.sh -- Version 1.43'
 #   Created by Jim Bodden -- 5/14/2021
-#   Modified by Jim Bodden on -- 5/16/2021
+#   Modified by Jim Bodden on -- 5/17/2021
 #
 #/******************************************************************************
 
-fconvertSetListToIndividualSongs () # Function converts a file with a setlist of songs to individual song files
-{ #echo fconvertSetListToIndividualSongs
+FconvertSetListToIndividualSongs () # Function converts a file with a setlist of songs to individual song files
+{ #echo FconvertSetListToIndividualSongs
 
 for FILE in t-*.txt
    do
@@ -24,7 +24,7 @@ for FILE in t-*.txt
             then
                 ((SC++));
                 TITLE="$( cut -d"[" -f1 <<< "$LINE" | xargs )"
-                SongFile="s-$TITLE"
+                SongFile="s-"${TITLE//[[:blank:]]/};
                 if [ ! -f "$SongFile.txt" ];
                 then
                     NAME="$SongFile"; i=0
@@ -44,15 +44,17 @@ for FILE in t-*.txt
         done < $FILE
     done
 
-} # End of fconvertSetListToIndividualSongs
+} # End of FconvertSetListToIndividualSongs
 
 
-fconvertSongTextToChordPro () # Function converts an individual song file from plain text to ChordPro format for MobileSheets
-{ #echo fconvertSongTextToChordPro
+FconvertSongTextToChordPro () # Function converts an individual song file from plain text to ChordPro format for MobileSheets
+{ #echo FconvertSongTextToChordPro
     SongDir="~/songs"
     for FILE in s-*.txt
     do
-	    SOURCE=$FILE; ## SOURCE="FieryLove.txt"
+	    SOURCE=$FILE; ### SOURCE="s-HolyWater.txt"
+
+        
         A="sed 's/A / [A] /g'| sed 's/Ab/ [Ab] /g'| sed 's/A#/ [A#] /g'| sed ':a;N;$!ba;s/ A\n/ [A] \n /g'"
         B="sed 's/B / [B] /g'| sed 's/Bb/ [Bb] /g'| sed 's/B#/ [B#] /g'| sed ':a;N;$!ba;s/ B\n/ [B] \n /g'"
         C="sed 's/C / [C] /g'| sed 's/Cb/ [Cb] /g'| sed 's/C#/ [C#] /g'| sed ':a;N;$!ba;s/ C\n/ [C] \n /g'"
@@ -69,16 +71,19 @@ fconvertSongTextToChordPro () # Function converts an individual song file from p
             ((C++))
             if [[ $C -eq 1 ]]; 
             then 
-                TITLE="$( cut -d '[' -f 1 <<< "$LINE" )"; echo "$TITLE" > FL1.txt; 
+                TITLE="$( cut -d '[' -f 1 <<< "$LINE" )"; echo "$TITLE" | tee FL1.txt; 
             else
                 SPLIT1="$( cut -d '[' -f 1 <<< "$LINE" )"; SPLIT2="$( cut -d '[' --complement -s -f1 <<< "$LINE" )"; 
-                echo "$SPLIT1" | xargs>> FL1.txt; echo "[$SPLIT2">> FL1.txt
+                echo "$SPLIT1" | xargs | tee -a FL1.txt; 
+                echo "[$SPLIT2" | tee -a FL1.txt
             fi
         done < ./SongChordPro.txt 
-        cp FL1.txt "M-$TITLE.txt"
+        cp FL1.txt "M-$TITLE-BASS.txt"
         echo "Completed!  $C Lines converted from generic text to ChordPro format"
     done
-} # End of fconvertSongTextToChordPro
+
+
+} # End of FconvertSongTextToChordPro
 
 
 ################################################
@@ -87,8 +92,8 @@ fconvertSongTextToChordPro () # Function converts an individual song file from p
 #                                              #
 ################################################
 
-fconvertSetListToIndividualSongs
-fconvertSongTextToChordPro
+FconvertSetListToIndividualSongs
+FconvertSongTextToChordPro
 
 exit 0
 
