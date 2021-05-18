78db60724118ad0cd173a9b5929f81370154aa0c
Jim Bodden <jim@pcshowme.com>
2021-05-18 09:19:48 -0500

    Version 1.53 -- Ready for a prod run...

--- ConvertToChordProFormat.sh
+++ ConvertToChordProFormat.sh
@@ -4,9 +4,9 @@
 #
 #   Bash Script to batch convert songs from generic text to ChordPro format"
 #
-VERSION='Script: ConvertToChordProFormat.sh -- Version 1.43'
+VERSION='Script: ConvertToChordProFormat.sh -- Version 1.53'
 #   Created by Jim Bodden -- 5/14/2021
-#   Modified by Jim Bodden on -- 5/17/2021
+#   Modified by Jim Bodden on -- 5/18/2021
 #
 #/******************************************************************************
 
@@ -62,8 +62,9 @@ FconvertSongTextToChordPro () # Function converts an individual song file from p
         E="sed 's/E / [E] /g'| sed 's/Eb/ [Eb] /g'| sed 's/E#/ [E#] /g'| sed ':a;N;$!ba;s/ E\n/ [E] \n /g'"
         F="sed 's/F / [F] /g'| sed 's/Fb/ [Fb] /g'| sed 's/F#/ [F#] /g'| sed ':a;N;$!ba;s/ F\n/ [F] \n /g'"
         G="sed 's/G / [G] /g'| sed 's/Gb/ [Gb] /g'| sed 's/G#/ [G#] /g'| sed ':a;N;$!ba;s/ G\n/ [G] \n /g'"
-        DASH="sed 's/-://'| sed 's/-/[-]/g'"
-        cat $SOURCE | eval $A | eval $B | eval $C | eval $D | eval $E | eval $F | eval $G | eval $DASH > SongChordPro.txt 
+        DASH="sed 's/Pre-Chorus/PreChorus/'| sed 's/-://'| sed 's/-/[-]/g'| sed 's/::/[::]/g'"
+        REM="sed 's/()-://g'| sed 's/() -:)//g'| sed 's/()://g'| sed 's/(...)//g'| sed 's/()//g'| sed 's/):/)/g'"
+        cat $SOURCE | eval $A | eval $B | eval $C | eval $D | eval $E | eval $F | eval $G | eval $DASH | eval $REM > SongChordPro.txt 
         
         C=0; IFS=$'\n'
         while read LINE     
@@ -74,18 +75,50 @@ FconvertSongTextToChordPro () # Function converts an individual song file from p
                 TITLE="$( cut -d '[' -f 1 <<< "$LINE" )"; echo "$TITLE" | tee FL1.txt; 
             else
                 SPLIT1="$( cut -d '[' -f 1 <<< "$LINE" )"; SPLIT2="$( cut -d '[' --complement -s -f1 <<< "$LINE" )"; 
-                echo "$SPLIT1" | xargs | tee -a FL1.txt; 
-                echo "[$SPLIT2" | tee -a FL1.txt
+                if test ! -z "$LINE" 
+                then
+                    echo "$SPLIT1" | xargs | tee -a FL1.txt; 
+                    echo "[$SPLIT2" | tee -a FL1.txt
+                fi
             fi
         done < ./SongChordPro.txt 
-        cp FL1.txt "M-$TITLE-BASS.txt"
+        FinalSongTitle="M-"${TITLE//[[:blank:]]/}"-BASS.txt";
+        #cp FL1.txt "M-$TITLE-BASS.txt"
+        cp FL1.txt ./$FinalSongTitle
         echo "Completed!  $C Lines converted from generic text to ChordPro format"
     done
-
-
 } # End of FconvertSongTextToChordPro
 
 
+FformatConvertedSong () # Function to better format converted song for MobileSheets
+{ #echo FformatConvertedSong
+
+    for FILE in M-*-BASS.txt
+    do
+	    FILENAME1=$FILE; ### FILENAME1="M-TrueReligion-BASS.txt"
+        FILENAME2=$(echo "$FILENAME1" | awk '{ print substr( $0, 1, length($0)-7 ) }')".txt"
+        mapfile -t SongArray < $FILENAME1  
+        SongArrayLength=${#SongArray[@]}
+        echo ${SongArray[0]} | tee $FILENAME2
+        echo " " | tee -a $FILENAME2
+        echo ${SongArray[1]} | tee -a $FILENAME2
+        N=2
+        while [ $N -lt "$SongArrayLength" ]
+        do 
+            if [[ "${SongArray[N+1]}" != "["* ]]
+            then
+                echo ${SongArray[N]} | tee -a $FILENAME2
+            else
+                echo " " | tee -a $FILENAME2
+                echo ${SongArray[N]} | tee -a $FILENAME2
+            fi 
+            ((N++))
+        done
+    done        
+} # End of FformatConvertedSong
+
+
+#/******************************************************************************
 ################################################
 #                                              #
 #  Actual program execution code starts here.  #
@@ -94,6 +127,7 @@ FconvertSongTextToChordPro () # Function converts an individual song file from p
 
 FconvertSetListToIndividualSongs
 FconvertSongTextToChordPro
+FformatConvertedSong
 
 exit 0
 
